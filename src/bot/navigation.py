eff76bed02e5aeff93f96ebb1df46868e09039ce
Uoduodihs <uoduodihs@gmail.com>
2025-07-02 04:09:25 +0800

    新增返回功能

--- src/bot/navigation.py
+++ src/bot/navigation.py
@@ -20,3 +20,21 @@ def push_navigation_stack(func):
         return await func(update, context, **kwargs)
 
     return wrapper
+
+
+async def go_back(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+
+    await query.answer()
+
+    if context.chat_data['nav_stack'] and len(context.chat_data['nav_stack']) > 1:
+        context.chat_data['nav_stack'].pop()
+        logger.info(f"Popped from stack. New depth: {len(context.chat_data['nav_stack'])}")
+
+        previous_menu = context.chat_data['nav_stack'][-1]
+        previous_func = previous_menu['func']
+        previous_kwargs = previous_menu['kwargs']
+
+        await previous_func(update, context, **previous_kwargs)
+    else:
+        await query.edit_message_text(text="here is top menu")
