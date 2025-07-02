580726004bb5b350bed3a78a377e6a93ae4044b7
Uoduodihs <uoduodihs@gmail.com>
2025-07-03 05:17:08 +0800

    任务管理模块不再需要获取可用账号数量

--- src/task/core.py
+++ src/task/core.py
@@ -13,20 +13,14 @@ async def create_campaign(boost_link_ids: List[int]):
     campaign_objs_count = await CampaignModel.filter(Q(status=0) | Q(status=1)).count()
     if campaign_objs_count > 0:
         return False, {'message': '当前有任务正在进行, 请等待任务运行完毕后再创建新任务'}
+
     campaigns = []
     for boost_link_id in boost_link_ids:
         boost_link = await BoostLinkModel.get_or_none(id=boost_link_id, is_deleted=False)
         if boost_link is None:
             return False, {'message': f'助力链接 {boost_link_id} 不存在'}
 
-        campaign = await CampaignModel.get_or_none(boost_link_id=boost_link_id, status=1)
-        if campaign is not None:
-            # return False, {'message': '此链接已存在进行中的任务, 请勿重复创建'}
-            continue
-
-        valid_accounts = await get_available_accounts(boost_link_id)
-
-        campaigns.append(CampaignModel(boost_link_id=boost_link_id, total_assigned=len(valid_accounts)))
+        campaigns.append(CampaignModel(boost_link_id=boost_link_id))
 
     try:
         await CampaignModel.bulk_create(campaigns)
