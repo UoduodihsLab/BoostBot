5622fec73698df78ecaa9c142aefa5f93dd9411e
Uoduodihs <uoduodihs@gmail.com>
2025-07-03 02:17:12 +0800

    不允许再有任务运行的情况下创建任务, 避免多个任务并发运行，竞争使用账号

--- src/task/core.py
+++ src/task/core.py
@@ -1,12 +1,18 @@
 from typing import List
+
+from tortoise.expressions import Q
+
 from models import *
-from src.utils.logger import get_console_logger
 from src.database import get_available_accounts
+from src.utils.logger import get_console_logger
 
 logger = get_console_logger()
 
 
 async def create_campaign(boost_link_ids: List[int]):
+    campaign_objs_count = await CampaignModel.filter(Q(status=0) | Q(status=1)).count()
+    if campaign_objs_count > 0:
+        return False, {'message': '当前有任务正在进行, 请等待任务运行完毕后再创建任务'}
     campaigns = []
     for boost_link_id in boost_link_ids:
         boost_link = await BoostLinkModel.get_or_none(id=boost_link_id, is_deleted=False)
