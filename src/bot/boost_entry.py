2639c4032dc1a422de3508f0bddf430d32f09394
Uoduodihs <uoduodihs@gmail.com>
2025-07-03 05:22:43 +0800

    移除存在进行中任务时不能创建新任务的限制

--- src/bot/boost_entry.py
+++ src/bot/boost_entry.py
@@ -9,9 +9,9 @@ from src.task.core import create_campaign
 
 async def start_boost(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(chat_id=update.effective_chat.id, text='正在创建助力任务...')
+
     boost_link_ids = context.chat_data['boost_link_ids']
     success, error_message = await create_campaign(boost_link_ids)
-
     if not success:
         await context.bot.send_message(update.effective_chat.id, error_message)
         return
@@ -20,5 +20,5 @@ async def start_boost(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 
     task = asyncio.create_task(schedule_tasks())
 
-    text = '助力任务已成功启动, 你可以输入命令 /list_tasks 查看任务实时进度'
+    text = '助力任务已成功启动, 你可以输入命令 /list_tasks 查看活动中的任务实时进度'
     await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
